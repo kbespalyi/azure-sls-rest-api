a85c06cc7064ab0e3b0517f065db69cff7044070
Khusein Bespalyi <khusein.bespalyi@otolane.com>
2019-12-24 14:26:40 -0500

    first init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Azure Functions
+
+<https://serverless.com/blog/serverless-azure-functions-v1/>
+
+Refer to [Serverless docs](https://serverless.com/framework/docs/providers/azure/guide/intro/) for more information.
---
+++ src/utils.js
@@ -0,0 +1,13 @@
+/** Gets the param from either the query string
+  * or body of request
+  */
+
+module.exports.getQueryOrBodyParam = (req, param) => {
+  const { query, body } = req;
+  if (query && query[param]) {
+    return query[param];
+  }
+  if (body && body[param]) {
+    return body[param];
+  }
+};
